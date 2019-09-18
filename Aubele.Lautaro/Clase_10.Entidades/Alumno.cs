35d5c23d2105e8391d6d223b4ef52ba6f3119cd1
alumno <alumno@utnfra.com.ar>
2019-09-18 12:02:46 -0300

    clase 10

---
+++ Aubele.Lautaro/Clase_10.Entidades/Alumno.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_10.Entidades
+{
+    public class Alumno
+    {
+        protected string apellido;
+        protected string nombre;
+        protected int legajo;
+        protected ETipoExamen examen;
+
+        
+        public enum ETipoExamen
+        {
+            Primero,Segundo,Final
+        }
+     
+        public int Legajo { get { return this.legajo;} }
+        public string Apellido { get { return this.apellido;} }
+        public string Nombre { get { return this.nombre;} }
+        public ETipoExamen Examen { get {return this.examen ;} }
+
+
+        public Alumno(string nombre,string apellido,int legajo,ETipoExamen examen)
+        {
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.legajo = legajo;
+            this.examen = examen;
+        }
+
+        public static string Mostrar(Alumno a)
+        {
+            return !Object.Equals(a, null) ? $"Nombre: {a.nombre} Apellido: {a.apellido}  Legajo: {a.legajo}  Examen: {a.examen}" : "ERROR";
+        }
+
+        public static bool operator ==(Alumno a, Alumno b)
+        {
+            bool retorno = false;
+
+            if(a.legajo == b.legajo)
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Alumno a,Alumno b)
+        {
+            return !(a == b);
+        }
+
+        public static int OrdenarPorApellidoAsc(Alumno a, Alumno b)
+        {
+            return string.Compare(a.apellido, b.apellido);
+        }
+        public static int OrdenarPorApellidoDesc(Alumno a, Alumno b)
+        {
+            return -1 * OrdenarPorApellidoAsc(a,b);
+
+        }
+        public static int OrdenarPorLegajoAsc(Alumno a, Alumno b)
+        {
+            int retorno = 0;
+            if (a.legajo > b.legajo)
+            {
+                retorno = -1;
+            }
+            else if (a.legajo < b.legajo)
+            {
+                retorno = 1;
+            }
+            return retorno;
+
+        }
+        public static int OrdenarPorLegajoDesc(Alumno a, Alumno b)
+        {
+            int retorno = 0;
+            if (a.legajo < b.legajo)
+            {
+                retorno = -1;
+            }
+            else if (a.legajo > b.legajo)
+            {
+                retorno = 1;
+            }
+            return retorno;
+
+        }
+    }
+}
---
+++ Aubele.Lautaro/Clase_10.Entidades/Catedra.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_10.Entidades
+{
+    class Catedra
+    {
+        private List<Alumno> alumnos;
+
+        public List<Alumno> Alumnos { get { return this.alumnos; } }
+
+        enum ETipoOrdenamiento
+        {
+            LegajoAscendente,
+            LegajoDescendente,
+            ApellidoAscendente,
+            ApellioDescendente
+        }
+
+        public Catedra()
+        {
+
+        }
+
+        public static bool operator !=(Catedra c, Alumno a)
+        {
+            return !(c == a);
+
+        }
+        public static bool operator -(Catedra c,Alumno a)
+        {
+            bool retorno = false;
+
+            if (c == a)
+            {
+                retorno = c.Alumnos.Remove(a);
+            }
+            return retorno;
+
+        }
+        public static int operator |(Alumno a, Catedra c)
+        {
+            int retorno = -1;
+
+            if (c == a)
+            {
+                retorno = c.Alumnos.IndexOf(a);
+            }
+            return retorno;
+        }
+        public static bool operator +(Catedra c, Alumno a)
+        {
+            bool retorno = false;
+
+            if(c != a)
+            {
+                retorno = true;
+                c.Alumnos.Add(a);
+            }
+            return retorno;
+
+        }
+        public static bool operator ==(Catedra c, Alumno a)
+        {
+            return (!Object.Equals(c, null) && !Object.Equals(a, null) ? c.Alumnos.Contains(a) : false);
+        }
+    }
+}
