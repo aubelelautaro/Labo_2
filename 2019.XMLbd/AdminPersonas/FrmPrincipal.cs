16b090675144a3c967c30647d6ed645191576b49
aubelelautaro <36264982+aubelelautaro@users.noreply.github.com>
2019-11-05 10:41:50 -0300

    masom1erda2

---
+++ 2019.XMLbd/AdminPersonas/FrmPrincipal.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Data.SqlClient;
+
+namespace AdminPersonas
+{
+    public partial class FrmPrincipal : Form
+    {
+        private List<Persona> lista;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+            this.IsMdiContainer = true;
+            this.WindowState = FormWindowState.Maximized;
+
+            this.lista = new List<Persona>();
+        }
+
+        private void cargarArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.openFileDialog1.ShowDialog();
+
+                XmlSerializer xml = new XmlSerializer(typeof(List<Persona>));
+                XmlTextReader xmltxt = new XmlTextReader(openFileDialog1.FileName);
+
+                
+                this.lista = (List<Persona>)xml.Deserialize(xmltxt);
+
+                xmltxt.Close();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void guardarEnArchivoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                this.saveFileDialog1.ShowDialog();
+
+                XmlSerializer xml = new XmlSerializer(typeof(List<Persona>));
+
+                XmlTextWriter xmltxt = new XmlTextWriter(saveFileDialog1.FileName, Encoding.UTF8);
+
+                xml.Serialize(xmltxt, this.lista);
+
+                xmltxt.Close();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+
+        private void visualizarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmVisorPersona frm = new frmVisorPersona(lista);
+
+            frm.StartPosition = FormStartPosition.CenterScreen;
+
+            frm.Show();
+            this.lista = frm.Lista;
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void conectarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection sql = new SqlConnection(Properties.Settings.Default.Conexion);
+
+                sql.Open();
+                MessageBox.Show("EXITO!");
+                SqlCommand comando = new SqlCommand();
+
+                comando.Connection = sql;
+                comando.CommandType = CommandType.Text;
+                comando.CommandText = "SELECT * FROM[personas_bd].[dbo].[personas]";
+
+                SqlDataReader dataReader = comando.ExecuteReader(); // no se puede hacer busquedas, solo va para adelante, TODOS LOS REGISTROS DE LA BASE DE DATOS VAN A ESTAR EN ESTE OBJETO, PERO SE LE UNA SOLA VEZ
+                //EL EXECTUE READER TRAE TODOS LOS DATOS DEL SERVIDOR Y LA IDEA ES RECUPERARLO, PASANDOLE LOS PARAMETROS AL CONSTRUCTOR DE LA LISTA A PARTIR DE LA BASE DE DATOS
+                while (dataReader.Read() != false)
+                {
+                    this.lista.Add(new Persona(dataReader[1].ToString(), dataReader[2].ToString(),Convert.ToInt32(dataReader[3])));
+
+                    MessageBox.Show($"Id: {dataReader[0].ToString()}\nNombre: {dataReader[1].ToString()}\nApellido: {dataReader[2].ToString()}\nEdad: {dataReader[3].ToString()}");
+                    /*MessageBox.Show(dataReader[0].ToString()); id
+                    MessageBox.Show(dataReader[1].ToString()); nombre
+                    MessageBox.Show(dataReader[2].ToString()); apellido
+                    MessageBox.Show(dataReader[3].ToString()); edad
+                    */
+                }
+
+                comando.Connection.Close();
+                dataReader.Close();
+                sql.Close();
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+        }
+    }
+}
---
+++ 2019.XMLbd/AdminPersonas/frmVisorPersona.cs
@@ -0,0 +1,141 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AdminPersonas
+{
+    public partial class frmVisorPersona : Form
+    {
+        private List<Persona> lista;
+
+        public List<Persona> Lista
+        {
+            get
+            {
+                return this.lista;
+            }
+        }
+
+        public frmVisorPersona(List<Persona> lista)
+        {
+            InitializeComponent();
+            this.lista = lista;
+            this.ActualizarLista();
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            frmPersona frm = new frmPersona();
+            frm.StartPosition = FormStartPosition.CenterScreen;
+
+            frm.ShowDialog();
+            if(frm.DialogResult == DialogResult.OK)
+            {
+                this.lista.Add(frm.Persona);
+                this.ActualizarLista();
+
+                try
+                {
+                    SqlCommand comando = new SqlCommand();
+                    SqlConnection sql = new SqlConnection(Properties.Settings.Default.Conexion);
+
+                    sql.Open();
+                    comando.Connection = sql;
+                   
+                    comando.CommandType = CommandType.Text;
+
+                    comando.CommandText = $"INSERT INTO Personas(nombre,apellido,edad) VALUES('{frm.Persona.nombre}','{frm.Persona.apellido}',{frm.Persona.edad})";
+                    comando.ExecuteNonQuery();
+                    //TEXTO Q SE LE PASA AL COMMAND TEXT values('COMILLAS SIMPLES EN LOS STRING','APELLIDO',31) // listado dentro de un try catch
+                    //PROBLEMA: CONCATENAR CADENA..........
+
+
+                    comando.Connection.Close();
+                    sql.Close();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message);
+                }
+                
+            }
+
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            if(lstVisor.SelectedIndex >=0)
+            {
+                Persona per = lista[index];
+                frmPersona frm = new frmPersona(per);
+                frm.StartPosition = FormStartPosition.CenterScreen;
+                frm.ShowDialog();
+                if(frm.DialogResult == DialogResult.OK)
+                {
+                    this.lista.Remove(per);
+                    this.lista.Add(frm.Persona);
+
+                    try
+                    {
+                        SqlCommand comando = new SqlCommand();
+                        SqlConnection sql = new SqlConnection(Properties.Settings.Default.Conexion);
+
+                        sql.Open();
+                        comando.Connection = sql;
+
+                        comando.CommandType = CommandType.Text;
+
+                        comando.CommandText = $"UPDATE Personas SET nombre = '{frm.Persona.nombre}' WHERE id = index+1" +
+                            $"UPDATE Personas SET apellido = '{frm.Persona.apellido}' WHERE id = index+1" +
+                            $"UPDATE Personas SET edad = '{frm.Persona.edad}' WHERE id = index+1";
+
+
+                        comando.ExecuteNonQuery();
+
+                        comando.Connection.Close();
+                        sql.Close();
+                    }
+                    catch (Exception exc)
+                    {
+                        MessageBox.Show(exc.Message);
+                    }
+                }
+                this.ActualizarLista();
+            }
+            
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            frmPersona frm = new frmPersona();
+            frm.StartPosition = FormStartPosition.CenterScreen;
+            int index = this.lstVisor.SelectedIndex;
+
+            if(index >=0)
+            {
+                this.lista.Remove(lista[index]);
+                this.ActualizarLista();
+            }
+            
+        }
+
+        private void ActualizarLista()
+        {
+            this.lstVisor.Items.Clear();
+            foreach (Persona value in this.lista)
+            {
+                this.lstVisor.Items.Add(value.ToString());
+            }
+        }
+    }
+}
